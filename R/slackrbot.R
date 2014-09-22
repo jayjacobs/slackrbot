0afda56939947cecb31a9b638726542b4b9ea811
jayjacobs <jay@TUSVA079ML0400L.local>
2014-09-22 11:30:23 -0500

    weird bug, no response at all.

--- R/slackrbot.R
+++ R/slackrbot.R
@@ -71,6 +71,7 @@ parseRequest <- function(post) {
       response <- dunno(post)
     }
   }
+  response
 }
 
 #' look for a message to me, or if I spoke recently
@@ -95,39 +96,6 @@ setupPost <- function(post) {
   post
 }
 
-#' placeholder function with notes
-oldfunc <- function() {
-    #user_id => USLACKBOT 
-    # user_name => slackbot 
-
-    
-    
-#     if (!is.null(zipit$user_name)) {
-#       if (zipit$user_name == "jayjacobs") {
-#         response <- paste0(zipit$user_name, ": zip it.")
-#         body <- toJSON(list("text"=response))
-#       }
-#     }
-#   }
-  
-  # $token <- "d9W84OJWBaTUOutg99701Oal"
-  # $team_id <- "T02J411CY"
-  # $team_domain <- "cyberlab"
-  # $service_id <- "2689161008"
-  # $channel_id <- "C02J4F7LL"
-  # $channel_name <- "code"
-  # $timestamp <- "1411175198.000544"
-  # $user_id <- "U02J36J49"
-  # $user_name <- "jayjacobs"
-  # $text <- "test"
-  list(
-    status = 200L,
-    headers = list(
-      'Content-Type' = 'text/html'
-    ),
-    body = body
-  )
-}
 
 #' Records the request information from the session
 #' 
